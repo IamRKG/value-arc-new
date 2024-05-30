49f437cf4146ea886fbf4cf7416a901fca87dcf1
IamRKG <radhacreative@gmail.com>
2024-05-30 17:00:19 +0530

    First Commit

---
+++ src/components/Swiper.jsx
@@ -0,0 +1,83 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/zoom";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import "./styles.css";
+
+// import required modules
+import { Zoom, Navigation, Pagination } from "swiper/modules";
+
+export default function HomeSwiper() {
+  return (
+    <>
+      <Swiper
+        style={{
+          "--swiper-navigation-color": "#fff",
+          "--swiper-pagination-color": "#fff",
+        }}
+        zoom={true}
+        navigation={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Zoom, Navigation, Pagination]}
+        className="swiper"
+      >
+        <SwiperSlide>
+          <img src="../src/assets/Egale.png" alt="asdas" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../src/assets/Home-page-image_1.png" alt="asdas" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../src/assets/Home-page-image_2.png" alt="asdas" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../src/assets/Home-page-image_3.png" alt="asdas" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../src/assets/Home-page-image_4.png" alt="asdas" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../src/assets/Home-page-image_5.png" alt="asdas" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../src/assets/Home-page-image_6.png" alt="asdas" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../src/assets/Home-page-image_7.png" alt="asdas" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../src/assets/Home-page-image_8.png" alt="asdas" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../src/assets/Home-page-image_9.png" alt="asdas" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../src/assets/Home-page-image_10.png" alt="asdas" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../src/assets/Home-page-image_11.png" alt="asdas" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../src/assets/Home-page-image_12.png" alt="asdas" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../src/assets/Home-page-image_13.png" alt="asdas" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../src/assets/Home-page-image_14.png" alt="asdas" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../src/assets/Home-page-image_15.png" alt="asdas" />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
---
+++ src/components/Wave.jsx
@@ -0,0 +1,13 @@
+import './wave.css'
+const Wave = () => {
+  return (
+    <>
+    <div class="ocean">
+  <div class="wave"></div>
+  <div class="wave"></div>
+</div>
+    </>
+  );
+};
+
+export default Wave;
