faada1f8d646cabdc7c94f678dc5ab1d90e65841
IamRKG <radhacreative@gmail.com>
2024-05-30 17:25:31 +0530

    Public Image

--- src/components/Swiper.jsx
+++ src/components/Swiper.jsx
@@ -30,52 +30,52 @@ export default function HomeSwiper() {
         className="swiper"
       >
         <SwiperSlide>
-          <img src="../src/assets/images/Egale.png" alt="asdas" />
+          <img src="../assets/images/Egale.png" alt="asdas" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="../src/assets/images/Home-page-image_1.png" alt="asdas" />
+          <img src="../assets/images/Home-page-image_1.png" alt="asdas" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="../src/assets/images/Home-page-image_2.png" alt="asdas" />
+          <img src="../assets/images/Home-page-image_2.png" alt="asdas" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="../src/assets/images/Home-page-image_3.png" alt="asdas" />
+          <img src="../assets/images/Home-page-image_3.png" alt="asdas" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="../src/assets/images/Home-page-image_4.png" alt="asdas" />
+          <img src="../assets/images/Home-page-image_4.png" alt="asdas" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="../src/assets/images/Home-page-image_5.png" alt="asdas" />
+          <img src="../assets/images/Home-page-image_5.png" alt="asdas" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="../src/assets/images/Home-page-image_6.png" alt="asdas" />
+          <img src="../assets/images/Home-page-image_6.png" alt="asdas" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="../src/assets/images/Home-page-image_7.png" alt="asdas" />
+          <img src="../assets/images/Home-page-image_7.png" alt="asdas" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="../src/assets/images/Home-page-image_8.png" alt="asdas" />
+          <img src="../assets/images/Home-page-image_8.png" alt="asdas" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="../src/assets/images/Home-page-image_9.png" alt="asdas" />
+          <img src="../assets/images/Home-page-image_9.png" alt="asdas" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="../src/assets/images/Home-page-image_10.png" alt="asdas" />
+          <img src="../assets/images/Home-page-image_10.png" alt="asdas" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="../src/assets/images/Home-page-image_11.png" alt="asdas" />
+          <img src="../assets/images/Home-page-image_11.png" alt="asdas" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="../src/assets/images/Home-page-image_12.png" alt="asdas" />
+          <img src="../assets/images/Home-page-image_12.png" alt="asdas" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="../src/assets/images/Home-page-image_13.png" alt="asdas" />
+          <img src="../assets/images/Home-page-image_13.png" alt="asdas" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="../src/assets/images/Home-page-image_14.png" alt="asdas" />
+          <img src="../assets/images/Home-page-image_14.png" alt="asdas" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="../src/assets/images/Home-page-image_15.png" alt="asdas" />
+          <img src="../assets/images/Home-page-image_15.png" alt="asdas" />
         </SwiperSlide>
       </Swiper>
     </>
